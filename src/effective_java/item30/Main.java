291567cc31fccd5de76ae3961273c94bb6c05405
kimdozzi <neo5188@gmail.com>
2024-06-10 16:00:49 +0900

    item02를 보면서 언급된 item03 살짝 맛보기

--- src/effective_java/item30/Main.java
+++ src/effective_java/item30/Main.java
@@ -1,2 +1,16 @@
-package effective_java.item30;public class Main {
+package effective_java.item30;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static Set union(Set s1, Set s2) {
+        Set result = new HashSet(s1);
+        result.addAll(s2);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        union(new HashSet(){}, new HashSet(){});
+    }
 }
