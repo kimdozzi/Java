2dbdf15f552dd74df4613c90def109f7eb20b2d7
kimdozzi <neo5188@gmail.com>
2024-08-02 19:58:06 +0900

    dx 특강 코드 배틀

--- src/algorithm/삼성전자dx특강/코드배틀/MagicRoom.java
+++ src/algorithm/삼성전자dx특강/코드배틀/MagicRoom.java
@@ -1,4 +1,88 @@
 package algorithm.삼성전자dx특강.코드배틀;
 
-public class MagicRoom {
+import java.util.Scanner;
+import java.util.Random;
+
+class MagicRoom {
+    private final static int NUM_PEOPLE = 1000000;
+    private final static int MAX_ROOM = 1000000;
+
+    private static int SCORE = 0;
+    private boolean room[] = new boolean[MAX_ROOM];
+    private boolean citizen[] = new boolean[NUM_PEOPLE];
+    private boolean arrested[] = new boolean[NUM_PEOPLE];
+
+    public void putln(int n, int a) {
+        if (n < 0 || n >= MAX_ROOM) return;
+        if (a < 0 || a >= NUM_PEOPLE) return;
+        if (citizen[a]) room[n] = true;
+    }
+
+    public int closeDoor(int n) {
+        SCORE += 1;
+        if (n < 0 || n >= MAX_ROOM) return 0;
+        return (room[n] ? 1 : 0);
+    }
+    public void clearRoom() {
+        for(int i = 0; i < NUM_PEOPLE; i++) room[i] = false;
+    }
+    public void arrest(int a) {
+        SCORE += 10;
+        if (a < 0 || a >= NUM_PEOPLE) return;
+        if (citizen[a] == false) {
+            SCORE += 10000;
+        }
+        arrested[a] = true;
+    }
+
+    public int getScore(){
+        int score = SCORE;
+        for (int i = 0; i < NUM_PEOPLE; i++) {
+            if(citizen[i] == true && arrested[i] == false) {
+                score += 90000;
+            }
+        }
+        return score;
+    }
+    public MagicRoom() {
+        Random random = new Random();
+
+        SCORE = 0;
+        for (int i = 0; i < MAX_ROOM; i++) {
+            room[i] = false;
+        }
+        for (int i = 0; i < NUM_PEOPLE; i++) {
+            citizen[i] = false;
+            arrested[i] = false;
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            int index = random.nextInt(NUM_PEOPLE);
+            if (citizen[index]) {
+                i--;
+                continue;
+            }
+            citizen[index] = true;
+        }
+    }
+}
+
+class Solution {
+    private static UserSolution userSolution = new UserSolution();
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int TC = sc.nextInt();
+        int totalScore = 0;
+
+        for(int test = 1; test <= TC; test++) {
+            MagicRoom api = new MagicRoom();
+            userSolution.investigate(api);
+            int score = api.getScore();
+            if (score < 86400) System.out.println("#" + test + " 100 " + score);
+            else System.out.println("#" + test + " 0 " + score);
+
+            totalScore += score;
+        }
+        System.out.println("TOTAL SCORE : " + totalScore);
+    }
 }
--- src/algorithm/삼성전자dx특강/코드배틀/UserSolution.java
+++ src/algorithm/삼성전자dx특강/코드배틀/UserSolution.java
@@ -1,4 +1,46 @@
 package algorithm.삼성전자dx특강.코드배틀;
 
-public class UserSolution {
+import java.util.ArrayList;
+import java.util.List;
+
+class UserSolution {
+    private final static int NUM_PEOPLE = 1000000;
+    private final static int MAX_ROOM = 1000000;
+
+    void investigate(MagicRoom api) {
+        // 1. 100만명의 사람들을 square N 개의 그룹으로 나눔. (sqrt composition)
+        int r = (int) Math.floor(Math.sqrt(NUM_PEOPLE)); // 그룹의 크기
+        int g = NUM_PEOPLE / r; // 그룹의 개수
+        if (NUM_PEOPLE % r != 0) {
+            g++;
+        }
+
+        List<Integer> d = new ArrayList<>();
+
+        int idx=0;
+        for(int i=0; i<g; i++) {
+            int start = i*r;
+            for(int j=start; j<start+r; j++) {
+                api.putln(idx, j);
+            }
+            if (api.closeDoor(idx) != 0) {
+                d.add(start);
+                idx++;
+            }
+        }
+
+
+        idx = 0;
+        api.clearRoom();
+        for(int i=0; i<d.size(); i++) {
+            int start = d.get(i);
+            for(int j=start; j<start+r; j++) {
+                api.putln(idx, j);
+                if (api.closeDoor(idx) != 0) {
+                    api.arrest(j);
+                    idx++;
+                }
+            }
+        }
+    }
 }
