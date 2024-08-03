9ab48c7f29442d3ab482aa0472bd8bad4befc29b
kimdozzi <neo5188@gmail.com>
2024-08-04 01:37:09 +0900

    dx특강 day04 수만들기

---
+++ src/algorithm/삼성전자dx특강/day04/수만들기/Main.java
@@ -0,0 +1,10 @@
+package algorithm.삼성전자dx특강.day04.수만들기;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 563;
+        int n = 987654321 / x;
+        int m = 987654321 % x;
+        System.out.println(n + " " + m);
+    }
+}
--- src/algorithm/삼성전자dx특강/day04/수만들기/Solution.java
+++ src/algorithm/삼성전자dx특강/day04/수만들기/Solution.java
@@ -1,69 +1,84 @@
 package algorithm.삼성전자dx특강.day04.수만들기;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.Queue;
-
+import java.util.Scanner;
 
 class Solution {
+    static int N, K;
 
     public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
 
-        for (int tc = 1; tc <= T; ++tc) {
-            int N = Integer.parseInt(br.readLine());
-            int[] A = new int[N];
-            String[] arr = br.readLine().split(" ");
-            int K = Integer.parseInt(br.readLine());
-
+        for (int test_case = 1; test_case <= T; test_case++) {
+            // init
+            N = sc.nextInt();
+            int[] arr = new int[N];
             for (int i = 0; i < N; i++) {
-                A[i] = Integer.parseInt(arr[i]);
+                arr[i] = sc.nextInt();
             }
+            K = sc.nextInt();
 
-            Queue<Node> pq = new PriorityQueue<>();
-            pq.add(new Node(K, 0));
-
-            int cnt = K;
-
-            while (!pq.isEmpty()) {
-                Node n = pq.poll();
-
-                if (n.left == 0) {
-                    cnt = n.cnt;
-                    break;
-                }
-
-                pq.add(new Node(0, n.cnt + n.left));
-
-                for (int i = 0; i < N; i++) {
-                    pq.add(new Node(n.left / A[i], n.cnt + n.left % A[i]));
-                }
-            }
-
-            System.out.println("#" + tc + " " + cnt);
+            // solve
+            int res = solve(arr);
+            System.out.println(res);
         }
+    }
+
+    static int solve(int[] a) {
+        Queue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(K, 0));
+
+        int cnt = K;
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            if (node.leftNumber == 0) {
+                cnt = node.cnt;
+                break;
+            }
+
+            // x와 k의 차이를 3의 배수로 만든 후 d에 3을 곱한다.
+            // x에 1을 두번 더하고, d에 3을 곱한다.
+
+            // ex)
+            // k = 11
+            // x = 0, d = 1
+            // arr = [2,3]
+            // d(=1)에 2,3을 곱하던
+            // d(=2)에 2,3을 곱하던
+            // d(1*2) 에 2,3을 곱하던
+            // d(1*3) 에 2,3을 곱하던 모두 arr 배열의 원소 arr[i] 값들의 배수 -> d는 필요없음
+
+            pq.add(new Node(0, node.cnt + node.leftNumber));
+            for (int i = 0; i < N; i++) {
+                if (node.leftNumber >= a[i]) {
+                    pq.add(new Node(node.leftNumber / a[i], node.cnt + node.leftNumber % a[i]));
+                }
+            }
+        }
+        return cnt;
     }
 
     static class Node implements Comparable<Node> {
-        int left, cnt;
+        int leftNumber = 0;
+        int cnt = 0;
 
-        public Node(int left, int cnt) {
-            this.left = left;
+        public Node(int leftNumber, int cnt) {
+            this.leftNumber = leftNumber;
             this.cnt = cnt;
         }
 
-        public int compareTo(Node n) {
-            if (this.cnt > n.cnt) {
+        @Override
+        public int compareTo(Node o1) {
+            if (this.cnt > o1.cnt) {
                 return 1;
-            }
-            if (this.cnt == n.cnt) {
+            } else if (this.cnt == o1.cnt) {
                 return 0;
+            } else {
+                return -1;
             }
-
-            return -1;
         }
     }
 }
-
