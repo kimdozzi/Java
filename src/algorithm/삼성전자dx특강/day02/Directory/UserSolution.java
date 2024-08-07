8e670a51b70bf7ee0a2f5328ec652df242d26692
kimdozzi <neo5188@gmail.com>
2024-08-07 20:36:47 +0900

    dx특강 b형기출 문제 해결

--- src/algorithm/삼성전자dx특강/day02/Directory/UserSolution.java
+++ src/algorithm/삼성전자dx특강/day02/Directory/UserSolution.java
@@ -1,210 +1,186 @@
 package algorithm.삼성전자dx특강.day02.Directory;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
-class UserSolution {
-    private final static int NAME_MAXLEN = 6;
-    private final static int PATH_MAXLEN = 1999;
-    private final static int NODE_MAXLEN = 50001;
+public class UserSolution {
+    private static final int NAME_MAXLEN = 6;
+    private static final int PATH_MAXLEN = 1999;
 
-    int[] parent = new int[NODE_MAXLEN];
-    int[] cnt = new int[NODE_MAXLEN];
+    // 각 노드의 부모 노드, 각 노드의 자식 개수 + 1
+    private int[] parent = new int[50010];
+    private int[] cnt = new int[50010];
 
-    ArrayList<Set<Integer>> child = new ArrayList<>();
-    ArrayList<String> dirName = new ArrayList<>();
-    Queue<Integer> queue = new PriorityQueue<>(); // 사용할 수 있는 인덱스 저장
+    // 각 노드의 자식 노드 번호
+    private List<Map<String, Integer>> child = new ArrayList<>();
 
-    private static String getString(char[] path) {
-        StringBuilder sb = new StringBuilder();
-        int idx = 0;
-        while (true) {
-            if (path[idx] == '\0') {
-                break;
-            }
-            sb.append(path[idx++]);
+    // 각 노드의 이름
+    private List<String> dirName = new ArrayList<>();
+
+    // 사용할 수 있는 인덱스 저장
+    private Queue<Integer> que = new LinkedList<>();
+
+    // 경로 캐시
+    private Map<String, Integer> pathCache = new HashMap<>();
+
+    public static String convertCharArrayToString(char[] charArray) {
+        if (charArray == null) {
+            return null; // 입력이 null인 경우 null 반환
         }
-        return sb.toString();
+
+        // '\0' 문자가 있는지 확인하고 이를 제외한 길이 계산
+        int length = 0;
+        while (length < charArray.length && charArray[length] != '\0') {
+            length++;
+        }
+
+        // '\0' 문자가 없다면 전체 배열을 사용하여 문자열 생성
+        if (length == charArray.length) {
+            return new String(charArray);
+        }
+
+        // '\0' 문자가 있다면 해당 길이까지만 사용하여 문자열 생성
+        return new String(charArray, 0, length);
     }
 
+    // 사용할 수 있는 인덱스 하나씩 반환
+    private int getIdx() {
+        int t = que.poll();
+        return t;
+    }
+
+    // idx 노드와 그 부모 노드들의 cnt를 +C만큼 업데이트
+    private void updcnt(int idx, int C) {
+        cnt[idx] += C;
+        if (parent[idx] >= 0) {
+            updcnt(parent[idx], C);
+        }
+    }
+
+    // parent_node 아래에 child_node 삽입
     private void nodeInsert(int parentNode, int childNode) {
-        child.get(parentNode).add(childNode);
+        child.get(parentNode).put(dirName.get(childNode), childNode);
         parent[childNode] = parentNode;
         updcnt(parentNode, cnt[childNode]);
     }
 
-    private void updcnt(int parentNodeIdx, int childNodeDirectoryCount) {
-        // 상위 디렉토리에 하위 디렉토리의 총 개수를 저장.
-        cnt[parentNodeIdx] += childNodeDirectoryCount;
-        // 상위 디렉토리의 부모 디렉토리가 존재하면, 부모 디렉토리에게 개수 정보 전파
-        if (parent[parentNodeIdx] >= 0) {
-            updcnt(parent[parentNodeIdx], childNodeDirectoryCount);
-        }
-    }
-
-    private int getNewIdx() {
-        // 사용할 수 있는 인덱스 하나씩 반환.
-        return queue.poll();
-    }
-
-    // 파일의 경로가 주어졌을 때, 해당 노드를 찾아가는 함수
-    private int getPathIdx(String path) {
-        int ret = 0;
-        String[] parts = path.split("/");
-
-        for (String part : parts) {
-            if (part.isEmpty()) {
-                continue;
-            }
-
-            for (int i : child.get(ret)) {
-                if (dirName.get(i).equals(part)) {
-                    ret = i;
-                    break;
-                }
-            }
-        }
-        return ret;
-    }
-
-    private int copyNode(int srcIdx, int dstIdx) {
-        int newIdx = getNewIdx(); // 새로운 디렉토리 인덱스 번호를 가져옴.
-        cnt[newIdx] = cnt[srcIdx]; // 옮길 디렉토리의 총 개수를 새로운 디렉토리의 cnt에 추가.
-        dirName.set(newIdx, dirName.get(srcIdx)); // 디렉토리 이름 수정.
-
-        // 옮길 디렉토리의 자식 디렉토리 탐색.
-        for (int i : child.get(srcIdx)) {
-            // 자식 디렉토리들을 하나씩 복사.
-            int tmp = copyNode(i, newIdx);
-            child.get(newIdx).add(tmp);
-        }
-        parent[newIdx] = dstIdx;
-        return newIdx;
-    }
-
+    // idx 노드와 그 부모 노드 사이에 연결을 끊음
     private void unlink(int idx) {
         if (parent[idx] >= 0) {
             updcnt(parent[idx], -cnt[idx]);
-            child.get(parent[idx]).remove(idx);
+            child.get(parent[idx]).remove(dirName.get(idx));
         }
         parent[idx] = -1;
     }
 
-    void init(int n) {
-        queue.clear();
+    // dst 노드 아래에 src 노드와 같은 노드 복사
+    private int copyNode(int src, int dst) {
+        int newIdx = getIdx();
+        cnt[newIdx] = cnt[src];
+        dirName.set(newIdx, dirName.get(src));
+        child.set(newIdx, new HashMap<>());
+        for (int i : child.get(src).values()) {
+            int tmp = copyNode(i, newIdx);
+            child.get(newIdx).put(dirName.get(tmp), tmp);
+        }
+        parent[newIdx] = dst;
+        return newIdx;
+    }
+
+    // idx 노드와 그 자식 노드들 모두 삭제
+    private void delNode(int idx) {
+        for (int i : child.get(idx).values()) {
+            delNode(i);
+        }
+        child.get(idx).clear();
+        que.add(idx);
+        parent[idx] = -1;
+        cnt[idx] = 0;
+    }
+
+    // path가 가리키는 노드 번호 반환
+    private int getPathIdx(String path) {
+        if (pathCache.containsKey(path)) {
+            return pathCache.get(path);
+        }
+
+        int ret = 0;
+        String[] parts = path.split("/");
+        for (String part : parts) {
+            if (part.isEmpty()) {
+                continue;
+            }
+            if (!child.get(ret).containsKey(part)) {
+                return -1; // 경로가 존재하지 않는 경우
+            }
+            ret = child.get(ret).get(part);
+        }
+        pathCache.put(path, ret);
+        return ret;
+    }
+
+    // 초기화 메서드
+    public void init(int n) {
+        que.clear();
+        pathCache.clear();
         Arrays.fill(cnt, 0);
         Arrays.fill(parent, -1);
         child.clear();
         dirName.clear();
-
         for (int i = 0; i <= n; i++) {
-            child.add(new HashSet<>());
+            child.add(new HashMap<>());
             dirName.add("");
         }
         for (int i = 1; i <= n; i++) {
-            queue.add(i);
+            que.add(i);
         }
         cnt[0] = 1;
     }
 
-    void cmd_mkdir(char[] path, char[] name) {
-        String strPath = getString(path);
-        int dstNodeIdx = getPathIdx(strPath); // path의 인덱스 번호
-        int newNodeIdx = getNewIdx(); // 사용할 새로운 노드의 인덱스 번호
-        cnt[newNodeIdx] = 1; // 해당 디렉토리는 1개의 디렉토리를 가지고 있다. (=자기 자신)
-
-        String strName = getString(name);
-        dirName.set(newNodeIdx, strName); // 디렉토리 삽입 (새로운 인덱스(newNodeIdx) 위치에 name 이름을 가진 새로운 디렉토리를 추가한다.)
-        nodeInsert(dstNodeIdx, newNodeIdx);
+    // 디렉토리 생성 명령
+    public void cmd_mkdir(char[] path, char[] name) {
+        int t = getPathIdx(convertCharArrayToString(path));
+        if (t == -1) return; // 경로가 존재하지 않는 경우 무시
+        int newIdx = getIdx();
+        cnt[newIdx] = 1;
+        dirName.set(newIdx, convertCharArrayToString(name));
+        nodeInsert(t, newIdx);
+        pathCache.clear(); // 캐시를 지워서 일관성 유지
     }
 
-    void cmd_rm(char[] path) {
-        String strPath = getString(path);
-        int rmIdx = getPathIdx(strPath);
-        unlink(rmIdx);
-        delNode(rmIdx);
+    // 디렉토리 삭제 명령
+    public void cmd_rm(char[] path) {
+        int tidx = getPathIdx(convertCharArrayToString(path));
+        if (tidx == -1) return; // 경로가 존재하지 않는 경우 무시
+        unlink(tidx);
+        delNode(tidx);
+        pathCache.clear(); // 캐시를 지워서 일관성 유지
     }
 
-    private void delNode(int rmIdx) {
-        for (int i : child.get(rmIdx)) {
-            delNode(i);
-        }
-        child.get(rmIdx).clear();
-        queue.add(rmIdx);
-        parent[rmIdx] = -1;
-        cnt[rmIdx] = 0;
+    // 디렉토리 복사 명령
+    public void cmd_cp(char[] srcPath, char[] dstPath) {
+        int srcIdx = getPathIdx(convertCharArrayToString(srcPath));
+        int dstIdx = getPathIdx(convertCharArrayToString(dstPath));
+        if (srcIdx == -1 || dstIdx == -1) return; // 경로가 존재하지 않는 경우 무시
+        int getI = copyNode(srcIdx, dstIdx);
+        updcnt(dstIdx, cnt[getI]);
+        child.get(dstIdx).put(dirName.get(getI), getI);
+        pathCache.clear(); // 캐시를 지워서 일관성 유지
     }
 
-    void cmd_cp(char[] srcPath, char[] dstPath) {
-        String strSrcPath = getString(srcPath);
-        String strDstPath = getString(dstPath);
-        int srcIdx = getPathIdx(strSrcPath);
-        int dstIdx = getPathIdx(strDstPath);
-        int getIdx = copyNode(srcIdx, dstIdx);
-        updcnt(dstIdx, cnt[getIdx]);
-        child.get(dstIdx).add(getIdx);
-    }
-
-
-    void cmd_mv(char[] srcPath, char[] dstPath) {
-        String strSrcPath = getString(srcPath);
-        int srcIdx = getPathIdx(strSrcPath);
+    // 디렉토리 이동 명령
+    public void cmd_mv(char[] srcPath, char[] dstPath) {
+        int srcIdx = getPathIdx(convertCharArrayToString(srcPath));
+        int dstIdx = getPathIdx(convertCharArrayToString(dstPath));
+        if (srcIdx == -1 || dstIdx == -1) return; // 경로가 존재하지 않는 경우 무시
         unlink(srcIdx);
-        String strDstPath = getString(dstPath);
-        nodeInsert(getPathIdx(strDstPath), srcIdx);
+        nodeInsert(dstIdx, srcIdx);
+        pathCache.clear(); // 캐시를 지워서 일관성 유지
     }
 
-
-    int cmd_find(char[] path) {
-        return cnt[getPathIdx(getString(path))] - 1;
+    // 디렉토리 내 파일 개수 찾기 명령
+    public int cmd_find(char[] path) {
+        int idx = getPathIdx(convertCharArrayToString(path));
+        if (idx == -1) return -1; // 경로가 존재하지 않는 경우
+        return cnt[idx] - 1;
     }
 }
-
-//	 The below commented methods are for your reference. If you want
-//	 to use it, uncomment these methods.
-//
-//	int mstrcmp(char[] a, char[] b) {
-//		int i;
-//		for (i = 0; a[i] != '\0'; i++) {
-//			if (a[i] != b[i])
-//				return a[i] - b[i];
-//		}
-//		return a[i] - b[i];
-//	}
-//
-//	int mstrncmp(char[] a, char[] b, int len) {
-//		for (int i = 0; i < len; i++) {
-//			if (a[i] != b[i])
-//				return a[i] - b[i];
-//		}
-//		return 0;
-//	}
-//
-//	int mstrlen(char[] a) {
-//		int len = 0;
-//
-//		while (a[len] != '\0')
-//			len++;
-//
-//		return len;
-//	}
-//
-//	void mstrcpy(char[] dest, char[] src) {
-//		int i = 0;
-//		while (src[i] != '\0') {
-//			dest[i] = src[i];
-//			i++;
-//		}
-//		dest[i] = src[i];
-//	}
-//
-//	void mstrncpy(char[] dest, char[] src, int len) {
-//		for (int i = 0; i < len; i++) {
-//			dest[i] = src[i];
-//		}
-//		dest[len] = '\0';
-//	}
